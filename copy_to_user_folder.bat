6d30644c6732f597136eb51961f632d40ff28aca
RyanAHayden <microhayden@gmail.com>
2025-12-02 21:30:34 -0500

    copy to user folder

---
+++ copy_to_user_folder.bat
@@ -0,0 +1,24 @@
+@echo off
+REM Copy .config folder and komorebi.json to user directory
+
+set SOURCE_DIR=%~dp0
+set DEST_DIR=C:\Users\ryha
+
+REM Copy .config folder
+if exist "%SOURCE_DIR%.config" (
+    xcopy "%SOURCE_DIR%.config" "%DEST_DIR%\.config" /E /I /Y
+    echo .config folder copied successfully
+) else (
+    echo .config folder not found
+)
+
+REM Copy komorebi.json file
+if exist "%SOURCE_DIR%komorebi.json" (
+    copy "%SOURCE_DIR%komorebi.json" "%DEST_DIR%\komorebi.json" /Y
+    echo komorebi.json copied successfully
+) else (
+    echo komorebi.json not found
+)
+
+echo Done.
+pause
